e84f0a567b6ec6a2208a22d01c9d403c69dee81a
andys8 <andys8@users.noreply.github.com>
2023-02-21 15:38:41 +0100

    Parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "openai")]
+#[command(bin_name = "openai")]
+enum OpenAiCli {
+    OpenAiCliArgs(Args),
+}
+
+#[derive(clap::Args)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    manifest_path: Option<std::path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let OpenAiCli::OpenAiCliArgs(args) = OpenAiCli::parse();
+    println!("{:?}", args.manifest_path);
 }
